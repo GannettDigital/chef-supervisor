b658523ead205cda7410ef4c2a9468e9cc667f8a
John Crawford <john.crawford@crawfishpress.com>
2016-04-01 15:46:54 -0400

    GDAES-3467 more supervisord testing

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -10,13 +10,18 @@ if "#{os[:family]}" == 'smartos'
       it { should be_owned_by 'root' }
     end
 else
-    describe service('supervisor') do
-      it { should be_running }
-      it { should be_enabled }
-    end
-    describe file('/etc/init.d/supervisor') do
-      it { should exist }
-      it { should be_file }
-      it { should be_owned_by 'root' }
-    end
+  describe service('supervisor') do
+    it { should be_running }
+    it { should be_enabled }
+  end
+  describe file('/etc/init.d/supervisor') do
+    it { should exist }
+    it { should be_file }
+    it { should be_owned_by 'root' }
+  end
+  describe file('/var/log/tailgun/tailgun.out.log') do
+    it { should exist }
+    it { should be_file }
+    its(:content) { should contain('Lorem ipsum dolor sit amet') }
+  end
 end
